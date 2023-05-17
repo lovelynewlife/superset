988e9017da4706793f6b88dc9b7cb1be8aacd79e
uw1 <newlife_1114752@163.com>
2023-05-17 08:01:31 -0700

    config: init npm packages and enable i18n chinese.

--- superset/config.py
+++ superset/config.py
@@ -359,11 +359,11 @@ BABEL_DEFAULT_LOCALE = "en"
 BABEL_DEFAULT_FOLDER = "superset/translations"
 # The allowed translation for you app
 LANGUAGES = {
+    "zh": {"flag": "cn", "name": "Chinese"},
     "en": {"flag": "us", "name": "English"},
     "es": {"flag": "es", "name": "Spanish"},
     "it": {"flag": "it", "name": "Italian"},
     "fr": {"flag": "fr", "name": "French"},
-    "zh": {"flag": "cn", "name": "Chinese"},
     "ja": {"flag": "jp", "name": "Japanese"},
     "de": {"flag": "de", "name": "German"},
     "pt": {"flag": "pt", "name": "Portuguese"},
@@ -376,7 +376,10 @@ LANGUAGES = {
 }
 # Turning off i18n by default as translation in most languages are
 # incomplete and not well maintained.
-LANGUAGES = {}
+LANGUAGES = {
+    "zh": {"flag": "cn", "name": "Chinese"},
+    "en": {"flag": "us", "name": "English"},
+}
 
 
 # Override the default d3 locale format
